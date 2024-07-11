b666330fda3cabd8ee523e6c5bcba46074a2c7d8
Gabriel Ibenye <gabrielibenye@gmail.com>
2024-07-11 03:28:15 -0400

    Added script to scale vote app every weekday

---
+++ scaler/scale-vote-hpa/scale_hpa.py
@@ -0,0 +1,24 @@
+import datetime
+from kubernetes import client, config
+
+def scale_hpa():
+    config.load_kube_config()
+    api_instance = client.AutoscalingV1Api()
+
+    today = datetime.datetime.now().weekday()  # Monday is 0, Sunday is 6
+    namespace = "voting-app"
+    hpa_name = "vote"
+
+    if today in [0, 1, 2, 3, 4]:  # Weekdays (Monday to Friday)
+        target_replicas = 5  
+    else:
+        target_replicas = 1  # Weekend scale down example
+
+    # Update HPA object
+    hpa = api_instance.read_namespaced_horizontal_pod_autoscaler(hpa_name, namespace)
+    hpa.spec.min_replicas = target_replicas
+    hpa.spec.max_replicas = target_replicas * 2  
+    api_instance.replace_namespaced_horizontal_pod_autoscaler(hpa_name, namespace, hpa)
+
+if __name__ == "__main__":
+    scale_hpa()
